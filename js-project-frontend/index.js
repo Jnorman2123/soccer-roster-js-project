1aac86403f2d8047550eebff2d6ec16347bc8a18
Jnorman2123 <Jnorman2123@gmail.com>
2020-02-12 15:49:58 -0500

    added league division element

--- js-project-frontend/index.js
+++ js-project-frontend/index.js
@@ -17,13 +17,16 @@ function renderLeague(league) {
     const leagueName = document.createElement('h1')
     const leagueLogo = document.createElement('img')
     const leagueCountry = document.createElement('h2')
+    const leagueDivision = document.createElement('h4')
     docBody.appendChild(leagueCard)
     leagueCard.appendChild(leagueName)
     leagueCard.appendChild(leagueLogo)
     leagueCard.appendChild(leagueCountry)
+    leagueCard.appendChild(leagueDivision)
     leagueName.innerText = league.name
     leagueLogo.src = `${league.logo}`
     leagueCountry.innerText = league.country
+    leagueDivision.innerText = league.division + ' division'
 }
 
 function renderAllLeagues(leagues) {
