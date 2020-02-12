74be3e064a77cd16b2b0d8c6ccbe68b25d77d0f3
Jnorman2123 <Jnorman2123@gmail.com>
2020-02-12 15:27:27 -0500

    moved variable declarations and added map function to fetch to render all leagues

--- js-project-frontend/index.js
+++ js-project-frontend/index.js
@@ -1,8 +1,5 @@
 const leaguesUrl = 'http://localhost:3000/leagues'
 
-const leagueCard = document.createElement('div')
-const leagueName = document.createElement('h1')
-
 document.addEventListener('DOMContentLoaded', () => {
     console.log('this is working')
     
@@ -11,12 +8,15 @@ document.addEventListener('DOMContentLoaded', () => {
 fetch(leaguesUrl)
     .then(response => response.json())
     .then((leagueData) => {
-        console.log(leagueData[0].name)
-        renderLeague(leagueData[0])
+        leagueData.map(data => {
+            return renderLeague(data)
+        })
     })
 
 function renderLeague(data) {
     const docBody = document.querySelector('body')
+    const leagueCard = document.createElement('div')
+    const leagueName = document.createElement('h1')
     docBody.appendChild(leagueCard)
     leagueCard.appendChild(leagueName)
     leagueName.innerHTML = data.name
