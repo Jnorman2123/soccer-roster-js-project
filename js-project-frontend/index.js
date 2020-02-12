b4ae5b4ab04452ad8fb5f437213b085d4498757f
Jnorman2123 <Jnorman2123@gmail.com>
2020-02-12 15:24:27 -0500

    added basic render league function as well as created new div and h1 elements

--- js-project-frontend/index.js
+++ js-project-frontend/index.js
@@ -1,9 +1,23 @@
-const baseUrl = 'http://localhost:3000/'
+const leaguesUrl = 'http://localhost:3000/leagues'
+
+const leagueCard = document.createElement('div')
+const leagueName = document.createElement('h1')
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('this is working')
-    fetch(baseUrl + 'leagues')
+    
+})
+
+fetch(leaguesUrl)
     .then(response => response.json())
     .then((leagueData) => {
-        console.log(leagueData)
+        console.log(leagueData[0].name)
+        renderLeague(leagueData[0])
     })
-})
+
+function renderLeague(data) {
+    const docBody = document.querySelector('body')
+    docBody.appendChild(leagueCard)
+    leagueCard.appendChild(leagueName)
+    leagueName.innerHTML = data.name
+}
