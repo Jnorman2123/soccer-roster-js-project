6afe1c295000d562cd36b413396aa9015c54bc20
Jnorman2123 <Jnorman2123@gmail.com>
2020-02-13 13:39:23 -0500

    removed extra code and changed form element ids

--- js-project-frontend/index.js
+++ js-project-frontend/index.js
@@ -65,20 +65,18 @@ function createEditLeagueButton() {
 
 function createNewLeagueForm() {
     const newLeagueForm = document.createElement('form')
-
     const docBody = document.querySelector('body')
-    
     
     let html = `
         <form onsubmit="createNewLeague; return false;">
-        <label>Name: </label>
-        <input type="text" id="name"><br>
-        <label>Logo: </label>
-        <input type="text" id="logo"><br>
-        <label>Country: </label>
-        <input type="text" id="country"><br>
-        <label>Division: </label>
-        <input type="text" id="division"><br>
+        <label>League Name: </label>
+        <input type="text" id="league_name"><br>
+        <label>League Logo: </label>
+        <input type="text" id="league_logo"><br>
+        <label>League Country: </label>
+        <input type="text" id="league_country"><br>
+        <label>League Division: </label>
+        <input type="text" id="league_division"><br>
         <input type="submit" value="Add a new League">
     `
     
@@ -86,42 +84,36 @@ function createNewLeagueForm() {
     newLeagueForm.innerHTML = html
     docBody.appendChild(newLeagueForm)
     newLeagueForm.addEventListener('submit', createNewLeague)
-    // const nameLabel = document.createElement('label')
-    // const nameInput = document.createElement('input')
-    // const logoLabel = document.createElement('label')
-    // const logoInput = document.createElement('input')
-    // const countryLabel = document.createElement('label')
-    // const countryInput = document.createElement('input')
-    // const divisionLabel = document.createElement('label')
-    // const divisionInput = document.createElement('input')
-    // docBody.innerHTML = ''
     
-    // newLeagueForm.appendChild(nameLabel)
-    // newLeagueForm.appendChild(nameInput)
-    // newLeagueForm.appendChild(logoLabel)
-    // newLeagueForm.appendChild(logoInput)
-    // newLeagueForm.appendChild(countryLabel)
-    // newLeagueForm.appendChild(countryInput)
-    // newLeagueForm.appendChild(divisionLabel)
-    // newLeagueForm.appendChild(divisionInput)
-    // newLeagueForm.appendChild(newLeagueSubmit)
-    // nameLabel.innerText = 'League Name: '
-    // logoLabel.innerText = 'League Logo Url: '
-    // countryLabel.innerText = 'League Country: '
-    // divisionLabel.innerText = 'Division Number: '
-    // nameInput.setAttribute('id', 'league_name')
-    // nameInput.setAttribute('name', 'league_name')
-    // logoInput.setAttribute('id', 'league_logo')
-    // logoInput.setAttribute('name', 'league_logo')
-    // countryInput.setAttribute('id', 'league_country')
-    // countryInput.setAttribute('name', 'league_country')
-    // divisionInput.setAttribute('id', 'league_division')
-    // divisionInput.setAttribute('name', 'league_division')
-    // newLeagueSubmit.setAttribute('type', 'submit') 
-    // newLeagueSubmit.setAttribute('value', 'Add a new League')
 }
 
 function createNewLeague() {
+    event.preventDefault()
+    const league = {
+        name: document.getElementById('league_name').value,
+        logo: document.getElementById('league_logo').value,
+        country: document.getElementById('league_country').value,
+        division: document.getElementById('league_division').value
+    }
+
+    fetch(leaguesUrl, {
+        method: 'POST',
+        body: JSON.stringify(league),
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        return response.json()
+    })
+    .then(leagueData => {
+        console.log(leagueData) 
+        fetchLeagues()
+    })
+}
+
+function editLeague() {
     event.preventDefault()
     const league = {
         name: document.getElementById('name').value,
