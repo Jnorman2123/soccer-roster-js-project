a2c4517561679a6d81aaeef9a7a1935a3ccad84a
Jnorman2123 <Jnorman2123@gmail.com>
2020-02-13 15:30:02 -0500

    moved league render function to the league class

--- js-project-frontend/index.js
+++ js-project-frontend/index.js
@@ -28,26 +28,7 @@ function fetchLeague(league) {
         })
 }
 
-function renderLeague(league) {
-    const docBody = document.querySelector('body')
-    const leagueCard = document.createElement('div')
-    const leagueName = document.createElement('h1')
-    const leagueLogo = document.createElement('img')
-    const leagueCountry = document.createElement('h2')
-    const leagueDivision = document.createElement('h4')
-    docBody.appendChild(leagueCard)
-    leagueCard.appendChild(leagueName)
-    leagueCard.appendChild(leagueLogo)
-    leagueCard.appendChild(leagueCountry)
-    leagueCard.appendChild(leagueDivision)
-    leagueName.innerText = league.name
-    leagueLogo.src = `${league.logo}`
-    leagueCountry.innerText = league.country
-    leagueDivision.innerText = league.division + ' division'
-    leagueName.addEventListener('click', () => {
-        fetchLeague(league)
-    })
-}
+
 
 function renderAllLeagues(leagues) {
     leagues.map(league => {
--- js-project-frontend/src/league.js
+++ js-project-frontend/src/league.js
@@ -5,4 +5,23 @@ class League {
         this.country = country;
         this.division = division
     }
+
+    renderLeague() {
+        const leagueCard = document.querySelector('#league_card')
+        const leagueName = document.createElement('h1')
+        const leagueLogo = document.createElement('img')
+        const leagueCountry = document.createElement('h2')
+        const leagueDivision = document.createElement('h4')
+        leagueCard.appendChild(leagueName)
+        leagueCard.appendChild(leagueLogo)
+        leagueCard.appendChild(leagueCountry)
+        leagueCard.appendChild(leagueDivision)
+        leagueName.innerText = league.name
+        leagueLogo.src = `${league.logo}`
+        leagueCountry.innerText = league.country
+        leagueDivision.innerText = league.division + ' division'
+        leagueName.addEventListener('click', () => {
+            fetchLeague(league)
+        })
+    }
 }
