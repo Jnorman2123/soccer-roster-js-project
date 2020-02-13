7311f9986f0a980beb7c8cf189aab6bb64491605
Jnorman2123 <Jnorman2123@gmail.com>
2020-02-12 21:09:05 -0500

    trying to figure out the form submit

--- js-project-frontend/index.js
+++ js-project-frontend/index.js
@@ -1,22 +1,31 @@
 const leaguesUrl = 'http://localhost:3000/leagues'
 const newLeagueButton = document.createElement('button')
-const newLeagueSubmit = document.createElement('input')
+const newLeagueForm = document.createElement('form')
 
 document.addEventListener('DOMContentLoaded', () => {
     newLeagueButton.addEventListener('click', () => {
-        console.log('testing')
         createNewLeagueForm()
     })
-    fetch(leaguesUrl)
-        .then(response => response.json())
-        .then((leagueData) => {
-            return renderAllLeagues(leagueData)
-        })
+
+    newLeagueForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        console.log("testing")
+        createNewLeague()
+    })
+
+    fetchLeagues()
     
     createNewLeagueButton()
 })
 
 
+function fetchLeagues() {
+    fetch(leaguesUrl)
+        .then(response => response.json())
+        .then((leagueData) => {
+            return renderAllLeagues(leagueData)
+        })
+}
 
 function renderLeague(league) {
     const docBody = document.querySelector('body')
@@ -50,48 +59,65 @@ function createNewLeagueButton() {
 
 function createNewLeagueForm() {
     const docBody = document.querySelector('body')
-    const newLeagueForm = document.createElement('form')
-    const nameLabel = document.createElement('label')
-    const nameInput = document.createElement('input')
-    const logoLabel = document.createElement('label')
-    const logoInput = document.createElement('input')
-    const countryLabel = document.createElement('label')
-    const countryInput = document.createElement('input')
-    const divisionLabel = document.createElement('label')
-    const divisionInput = document.createElement('input')
+    
+    
+    let html = `
+        <form onsubmit="createNewLeague; return false;">
+        <label>Name: </label>
+        <input type="text" id="league_name"><br>
+        <label>Logo: </label>
+        <input type="text" id="league_logo"><br>
+        <label>Country: </label>
+        <input type="text" id="league_country"><br>
+        <label>Division: </label>
+        <input type="text" id="league_division"><br>
+        <input type="submit" value="Add a new League">
+    `
+    
     docBody.innerHTML = ''
+    newLeagueForm.innerHTML = html
     docBody.appendChild(newLeagueForm)
-    newLeagueForm.appendChild(nameLabel)
-    newLeagueForm.appendChild(nameInput)
-    newLeagueForm.appendChild(logoLabel)
-    newLeagueForm.appendChild(logoInput)
-    newLeagueForm.appendChild(countryLabel)
-    newLeagueForm.appendChild(countryInput)
-    newLeagueForm.appendChild(divisionLabel)
-    newLeagueForm.appendChild(divisionInput)
-    newLeagueForm.appendChild(newLeagueSubmit)
-    nameLabel.innerText = 'League Name: '
-    logoLabel.innerText = 'League Logo Url: '
-    countryLabel.innerText = 'League Country: '
-    divisionLabel.innerText = 'Division Number: '
-    nameInput.setAttribute('id', 'league_name')
-    nameInput.setAttribute('name', 'league_name')
-    logoInput.setAttribute('id', 'league_logo')
-    logoInput.setAttribute('name', 'league_logo')
-    countryInput.setAttribute('id', 'league_country')
-    countryInput.setAttribute('name', 'league_country')
-    divisionInput.setAttribute('id', 'league_division')
-    divisionInput.setAttribute('name', 'league_division')
-    newLeagueSubmit.setAttribute('type', 'submit') 
-    newLeagueSubmit.setAttribute('value', 'Add a new League')
+    // const nameLabel = document.createElement('label')
+    // const nameInput = document.createElement('input')
+    // const logoLabel = document.createElement('label')
+    // const logoInput = document.createElement('input')
+    // const countryLabel = document.createElement('label')
+    // const countryInput = document.createElement('input')
+    // const divisionLabel = document.createElement('label')
+    // const divisionInput = document.createElement('input')
+    // docBody.innerHTML = ''
+    
+    // newLeagueForm.appendChild(nameLabel)
+    // newLeagueForm.appendChild(nameInput)
+    // newLeagueForm.appendChild(logoLabel)
+    // newLeagueForm.appendChild(logoInput)
+    // newLeagueForm.appendChild(countryLabel)
+    // newLeagueForm.appendChild(countryInput)
+    // newLeagueForm.appendChild(divisionLabel)
+    // newLeagueForm.appendChild(divisionInput)
+    // newLeagueForm.appendChild(newLeagueSubmit)
+    // nameLabel.innerText = 'League Name: '
+    // logoLabel.innerText = 'League Logo Url: '
+    // countryLabel.innerText = 'League Country: '
+    // divisionLabel.innerText = 'Division Number: '
+    // nameInput.setAttribute('id', 'league_name')
+    // nameInput.setAttribute('name', 'league_name')
+    // logoInput.setAttribute('id', 'league_logo')
+    // logoInput.setAttribute('name', 'league_logo')
+    // countryInput.setAttribute('id', 'league_country')
+    // countryInput.setAttribute('name', 'league_country')
+    // divisionInput.setAttribute('id', 'league_division')
+    // divisionInput.setAttribute('name', 'league_division')
+    // newLeagueSubmit.setAttribute('type', 'submit') 
+    // newLeagueSubmit.setAttribute('value', 'Add a new League')
 }
 
 function createNewLeague() {
     const league = {
-        name: document.getElementById('name').value,
-        logo: document.getElementById('logo').value,
-        country: document.getElementById('country').value,
-        division: document.getElementById('division').value
+        name: document.getElementById('league_name').value,
+        logo: document.getElementById('league_logo').value,
+        country: document.getElementById('league_country').value,
+        division: document.getElementById('league_division').value
     }
 
     fetch(leaguesUrl, {
@@ -104,6 +130,7 @@ function createNewLeague() {
     })
     .then(response => response.json)
     .then(league => {
-        renderLeague(league)
+        console.log(league) 
+        fetchLeagues()
     })
 }
