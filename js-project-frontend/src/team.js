4737aaea857119b67ea4caff1b8f359492fac0b9
Jnorman2123 <Jnorman2123@gmail.com>
2020-02-13 22:32:10 -0500

    added team constructor method to team class

--- js-project-frontend/src/team.js
+++ js-project-frontend/src/team.js
@@ -1,3 +1,12 @@
 class Team {
-    
+    constructor(id, name, logo, nickname, stadium, manager, year_founded, league_id) {
+        this.id = id
+        this.name = name
+        this.logo = logo
+        this.nickname = nickname
+        this.stadium = stadium
+        this.manager = manager
+        this.year_founded = year_founded
+        this.league_id = league_id
+    }
 }
