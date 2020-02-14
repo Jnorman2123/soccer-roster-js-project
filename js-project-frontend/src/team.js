0354f72e3caa600cbe26ad5defc587c048b97aaa
Jnorman2123 <Jnorman2123@gmail.com>
2020-02-13 22:48:32 -0500

    added render team method to team class

--- js-project-frontend/src/team.js
+++ js-project-frontend/src/team.js
@@ -9,4 +9,38 @@ class Team {
         this.year_founded = year_founded
         this.league_id = league_id
     }
+
+    renderTeam() {
+        const docBody = document.querySelector('body')
+        const teamCard = document.createElement('div')
+        const teamName = document.createElement('h2')
+        const teamLogo = document.createElement('img')
+        const teamNickname = document.createElement('h3')
+        const teamStadium = document.createElement('h4')
+        const teamManager = document.createElement('p')
+        const teamYearFounded = document.createElement('p')
+        teamCard.setAttribute('id', 'team_card')
+        teamName.setAttribute('id', 'team_name')
+        teamLogo.setAttribute('id', 'team_logo')
+        teamNickname.setAttribute('id', 'team_nickname')
+        teamStadium.setAttribute('id', 'team_stadium')
+        teamManager.setAttribute('id', 'team_manager')
+        teamYearFounded.setAttribute('id', 'team_year_founded')
+        docBody.appendChild(leagueCard)
+        teamCard.appendChild(teamName)
+        teamCard.appendChild(teamLogo)
+        teamCard.appendChild(teamNickname)
+        teamCard.appendChild(teamStadium)
+        teamCard.appendChild(teamManager)
+        teamCard.appendChild(teamYearFounded)
+        teamName.innerText = this.name
+        teamLogo.src = `${this.logo}`
+        teamNickname.innerText = this.nickname
+        teamStadium.innerText = this.stadium
+        teamManager.innerText = this.manager 
+        teamYearFounded.innerText = 'Team founded in ' + this.year_founded
+        teamName.addEventListener('click', () => {
+            fetchTeam(this)
+        })
+    }
 }
